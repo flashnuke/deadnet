cea45c650cec0c9083a1752fce936c49586606a9
flashnuke <adanikel@gmail.com>
2022-08-15 01:40:59 +0300

    change def sleep to 5

--- utils/argparser.py
+++ utils/argparser.py
@@ -1,6 +1,10 @@
 import argparse
 import os
 
+_DEF_CIDR = 24
+_DEF_SLEEPTIME = 5
+_DEF_PREFLEN = 64
+
 
 def define_args():
     parser = argparse.ArgumentParser(description=f'Perform an ARP cache poison attack',
@@ -9,12 +13,12 @@ def define_args():
                         help="the name of the network interface (from `ifconfig`, i.e -> 'eth0')",
                         required=True)
 
-    parser.add_argument("-m", "--set-cidrlen", dest='cidrlen', type=int, metavar=(""), default=24,
-                        help="set the cidr length (default -> /24 which means 0-256",
+    parser.add_argument("-m", "--set-cidrlen", dest='cidrlen', type=int, metavar=(""), default=_DEF_CIDR,
+                        help=f"set the IPv4 subnet cidr length (default -> /{_DEF_CIDR}",
                         required=False)
 
-    parser.add_argument("-s", "--sleep-interval", dest='s_time', type=int, metavar=(""), default=1,
-                        help="set the sleep time between each arp poison attempt (default -> 1[sec])",
+    parser.add_argument("-s", "--sleep-interval", dest='s_time', type=int, metavar=(""), default=_DEF_SLEEPTIME,
+                        help=f"set the sleep time between each arp poison attempt (default -> {_DEF_SLEEPTIME}[sec])",
                         required=False)
 
     parser.add_argument("-g", "--set-gateway", dest='gateway', type=str, metavar=(""), default=None,
@@ -25,8 +29,8 @@ def define_args():
                         default=False, help="spoof IPv6 ra packets, causing a dead router attack"
                                             " (disabled by default)", required=False)
 
-    parser.add_argument("-pl", "--set-preflen", dest='preflen', type=int, metavar=(""), default=64,
-                        help="set the prefix length of the IPv6 subnet (default -> 64",
+    parser.add_argument("-pl", "--set-preflen", dest='preflen', type=int, metavar=(""), default=_DEF_PREFLEN,
+                        help=f"set the prefix length of the IPv6 subnet (default -> {_DEF_PREFLEN})",
                         required=False)
 
     return parser.parse_args()
