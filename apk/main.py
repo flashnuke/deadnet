0eca987bd14c082ac10de2dc3c6fcbcafc713492
flashnuke <adanikel@gmail.com>
2023-01-27 20:47:26 +0200

    st

--- apk/main.py
+++ apk/main.py
@@ -3,12 +3,13 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 import copy
+from kivy.uix.gridlayout import GridLayout
 
 from kivy.uix.boxlayout import BoxLayout
 
 
 class MainApp(App):
-    _GATEWAY = "a"
+    _GATEWAY = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
     def build(self):
         self._GATEWAY = "127.0.0.1"
         self.main_layout = BoxLayout(orientation="vertical")
@@ -22,24 +23,28 @@ class MainApp(App):
         self.main_layout.add_widget(self.symbol_label)  # add price label
         self.button_test = Button(text='Start',
                                      size_hint=(None, None),
-                                     size=(170, 170),
                                      pos_hint={'center_x': .5, 'center_y': .5})
         self.button_test.bind(on_press=self.on_start_press)
         self.main_layout.add_widget(self.button_test)
         self.gateway = None
 
-        # textinputs = TextInput(text='Hello world', multiline=False)
-        # textinputs.bind()
-        # self.main_layout.add_widget(textinputs)
-        x = self._GATEWAY
-        textinput = TextInput(text=x, multiline=False)
+        textinput = TextInput(text=self._GATEWAY, multiline=False,
+                              foreground_color=[1, 1, 0, 1],
+                              background_color=[0, 0, 0.15, 0.15],
+                              pos_hint={'center_x': .5, 'center_y': .5},
+                              size_hint=(None, None))
         textinput.bind(text=self.on_text)
         self.main_layout.add_widget(textinput)
 
+
         return self.main_layout
+    ##,
+                           #   background_color=[0, 0, 0, 0]
+    #
     def on_text(self, instance, value):
         self.gateway=value
         print(f'gateway -> { self.gateway}')
+
     def on_start_press(self, instance):
         print("test")
     def on_enter(self, instance, value=None):
