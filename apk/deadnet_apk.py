660b1698fbb667838ea9d5944ccf7ffda8474be4
flashnuke <adanikel@gmail.com>
2025-04-25 11:06:48 +0300

    add import

--- apk/deadnet_apk.py
+++ apk/deadnet_apk.py
@@ -8,6 +8,7 @@ import threading
 import platform as pt
 from kivy.clock import Clock
 from kivy.logger import Logger
+from Typing import Any
 
 from concurrent.futures import ThreadPoolExecutor
 from utils import *
