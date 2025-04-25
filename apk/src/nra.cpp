fccaa3149a99a7c5124eb9fa56348d1f16a8fcb8
flashnuke <adanikel@gmail.com>
2025-04-25 12:14:41 +0300

    remove prints

--- apk/src/nra.cpp
+++ apk/src/nra.cpp
@@ -15,24 +15,19 @@ struct nd_opt_slla {
 };
 
 int main(int argc, char* argv[]) {
+    // Usage: <hw_addr> <src_addr> <prefix> <prefix_len> <if_name> <sleep_seconds>
     if (argc != 7) {
-        std::cerr << "DeadNet: Usage: " << argv[0] << " <hw_addr> <src_addr> <prefix> <prefix_len> <if_name> <sleep_seconds>" << std::endl;
         return 1;
     }
 
     double sleep_duration = atof(argv[6]);
-    std::cout << "DeadNet: Running with sleep duration " << sleep_duration << " seconds" << std::endl;
 
     while (1) {
-        std::cout << "DeadNet: Creating raw socket" << std::endl;
         int sock = socket(PF_INET6, SOCK_RAW, IPPROTO_RAW);
         if (sock < 0) {
-            std::cerr << "DeadNet: socket creation failed" << std::endl;
             return 1;
         }
-        std::cout << "DeadNet: Binding to device " << argv[5] << std::endl;
         if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, argv[5], std::strlen(argv[5])) < 0) {
-            std::cerr << "DeadNet: failed to bind to device " << argv[5] << std::endl;
             return 1;
         }
 
@@ -66,7 +61,6 @@ int main(int argc, char* argv[]) {
         opt_slla.hdr.nd_opt_type = ND_OPT_SOURCE_LINKADDR;
         opt_slla.hdr.nd_opt_len = 1;
 
-        std::cout << "DeadNet: Parsing MAC address " << argv[1] << std::endl;
         sscanf(argv[1], "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
                &opt_slla.hw_addr[0], &opt_slla.hw_addr[1], &opt_slla.hw_addr[2],
                &opt_slla.hw_addr[3], &opt_slla.hw_addr[4], &opt_slla.hw_addr[5]);
@@ -120,12 +114,9 @@ int main(int argc, char* argv[]) {
         memcpy(buf + sizeof(ip_header) + sizeof(icmp_header), &prefix_info, sizeof(prefix_info));
         memcpy(buf + sizeof(ip_header) + sizeof(icmp_header) + sizeof(prefix_info), &opt_slla, sizeof(opt_slla));
 
-        std::cout << "DeadNet: Sending packet" << std::endl;
         if (sendto(sock, buf, sizeof(buf), 0, (struct sockaddr*)&dest_addr, sizeof(dest_addr)) < 0) {
-            std::cerr << "DeadNet: sendto failed" << std::endl;
             return 1;
         }
-        std::cout << "DeadNet: Packet sent" << std::endl;
 
         sleep((unsigned int)sleep_duration);
     }
